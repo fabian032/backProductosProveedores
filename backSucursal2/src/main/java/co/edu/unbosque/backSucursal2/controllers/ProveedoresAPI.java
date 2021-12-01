f6cc3b12143e528e0e514f1381b932f31820501c
fabia <fabia@DESKTOP-JFH1ONM>
2021-12-01 12:28:52 -0500

    hola

---
+++ backSucursal2/src/main/java/co/edu/unbosque/backSucursal2/controllers/ProveedoresAPI.java
@@ -0,0 +1,51 @@
+package co.edu.unbosque.backSucursal2.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.unbosque.backSucursal2.models.Proveedores;
+import co.edu.unbosque.backSucursal2.repositories.ProveedoresDAO;
+
+@RestController
+@CrossOrigin(origins ="*",methods = { RequestMethod.POST, RequestMethod.GET, RequestMethod.PUT,
+		RequestMethod.DELETE})
+@RequestMapping("/api/proveedores")
+public class ProveedoresAPI {
+	
+	@Autowired
+	private ProveedoresDAO proveedoresdao;
+	
+	@PostMapping("/guardar")
+	public  Proveedores guardar(@Validated @RequestBody Proveedores proveedores) {
+		return proveedoresdao.insert(proveedores);
+	}
+	
+	@GetMapping("/listar")
+	public List<Proveedores> listar(){
+		return proveedoresdao.findAll();
+	}
+	
+	@PutMapping("/actualizar")
+	public void actualizar(@RequestBody Proveedores proveedores) {
+		proveedoresdao.save(proveedores);
+		
+	}
+	
+	@DeleteMapping("/eliminar/{id}")
+	public void eliminar(@PathVariable String id) {
+		proveedoresdao.deleteById(id);
+	}
+
+}
